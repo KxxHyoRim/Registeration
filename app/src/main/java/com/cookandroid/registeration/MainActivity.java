5b50213efd6b13e65e480060ad36226a92f6dd27
Suhyeon204 <k0s3y0@naver.com>
2019-12-14 19:50:30 +0900

    u

--- app/src/main/java/com/cookandroid/registeration/MainActivity.java
+++ app/src/main/java/com/cookandroid/registeration/MainActivity.java
@@ -1,5 +1,8 @@
 package com.cookandroid.registeration;
 
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +11,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,25 +29,18 @@ public class MainActivity extends AppCompatActivity {
     private ListView noticeListView;
     private NoticeListAdapter adapter;
     private List<Notice> noticeList;
+    public static String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        userID=getIntent().getStringExtra("userID");
 
         noticeListView=(ListView) findViewById(R.id.noticeListView);
         noticeList = new ArrayList<Notice>();
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
-        noticeList.add(new Notice("공지사항입니다.","수현이", "2019-12-08"));
         adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
         noticeListView.setAdapter(adapter);
 
@@ -88,5 +93,82 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
             }
         });
+
+        new BackgroundTask().execute();
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String>
+    {
+        String target;
+
+        @Override
+        protected void onPreExecute(){
+            //super.onPreExecute();
+            target="https://youngwoo311.cafe24.com/NoticeList.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try{
+                URL url=new URL(target);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder=new StringBuilder();
+                while((temp=bufferedReader.readLine())!=null)
+                {
+                    stringBuilder.append(temp+"\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values){
+            super.onProgressUpdate();
+        }
+
+        @Override
+        public void onPostExecute(String result)
+        {
+            try{
+                JSONObject jsonObject=new JSONObject(result);
+                JSONArray jsonArray=jsonObject.getJSONArray("response");
+                int count=0;
+                String noticeContent, noticeName, noticeDate;
+                while(count<jsonArray.length()){
+                    JSONObject object=jsonArray.getJSONObject(count);
+                    noticeContent=object.getString("noticeContent");
+                    noticeName=object.getString("noticeName");
+                    noticeDate=object.getString("noticeDate");
+                    Notice notice=new Notice(noticeContent,noticeName, noticeDate);
+                    noticeList.add(notice);
+                    adapter.notifyDataSetChanged();
+                    count++;
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private long lastTimeBackPressed;
+
+    @Override
+    public void onBackPressed(){
+        if(System.currentTimeMillis()-lastTimeBackPressed<1500){
+            finish();
+            return;
+        }
+        Toast.makeText(this,"'뒤로' 버튼을 한 번 더 눌러 종료합니다.",Toast.LENGTH_SHORT);
+        lastTimeBackPressed=System.currentTimeMillis();
     }
 }
